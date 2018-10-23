2765b1bb375b3fd643df61331e2953a0ee3e44d5
Akiva Ranells <akranells@gmail.com>
2018-10-23 10:58:57 +0300

    added mongo schema with users and basic server

---
+++ models/user-model.js
@@ -0,0 +1,39 @@
+const mongoose = require ('mongoose');
+mongoose.connect('mongodb://localhost/users');
+
+let userSchema = new mongoose.Schema({
+    userName: String,
+    bestTagsTotalScoreHistory: Number,
+    tags: Array
+});
+
+let User = mongoose.model('user', userSchema);
+
+let akiva = new User({
+    userName: "Akiva FullStack",
+    bestTagsTotalScoreHistory: 255,
+    tags: ['man', 'dog', 'candle']
+})
+
+let alex = new User({
+    userName: "Alex FullStack",
+    bestTagsTotalScoreHistory: 100500,
+    tags: ['phone', 'leash', 'computer']
+})
+
+let gaia = new User({
+    userName: "Gaia FullStack",
+    bestTagsTotalScoreHistory: 999999910,
+    tags: ['cloud', 'cat', 'window']
+})
+
+akiva.save()
+alex.save()
+gaia.save()
+
+let query = User.find;
+User.find({}, function(err, user){
+    if(err)throw err;
+    console.log(user)
+})
+module.exports = User
