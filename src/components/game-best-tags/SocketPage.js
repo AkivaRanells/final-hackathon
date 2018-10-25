f7c8bbe97e0e0ac2325e7f50734614f0b578d5d3
Akiva Ranells <akranells@gmail.com>
2018-10-25 12:30:02 +0300

    need to revert because of git issue

--- src/components/game-best-tags/SocketPage.js
+++ src/components/game-best-tags/SocketPage.js
@@ -6,6 +6,7 @@ class SocketPage extends React.Component {
         super(props);
 
         this.state = {
+            status:
             timer: 60,
             username: '',
             message: '',
@@ -15,12 +16,12 @@ class SocketPage extends React.Component {
 
         this.socket = io('localhost:8080');
 
-        this.socket.on('chat message', function (data) {
-            this.addMessage(data);
+        this.socket.on('chat message', (msg)=> {
+            this.addMessage(msg);
         });
 
-        this.socket.on('timer',  (seconds) =>{
-            console.log(seconds);
+        this.socket.on('timer',  (status) =>{
+            console.log(status);
              this.setState({timer:seconds})
         });
 
@@ -28,22 +29,24 @@ class SocketPage extends React.Component {
 
 
 
-        // this.sendMessage = () => {
-        //     console.log("sending")
-        //     this.socket.emit('chat message', {
-        //         message: this.state.message
-        //     })
-        //     this.setState({ message: '' });
-        // }
+ 
     }
 
-    addMessage = () => {
-        // console.log(this.socket.id);
-        //send it to the server
+    sendMessage = () => {
+        console.log("sending")
         this.socket.emit('chat message', {
             message: this.state.message
         })
-        this.setState({ messages: [...this.state.messages, {username: this.socket.id, message: this.state.message}] },function(){console.log(this.state.messages);});
+        this.setState({ message: '' });
+    }
+
+    addMessage = (msg) => {
+        // console.log(this.socket.id);
+        //send it to the server
+        // this.socket.emit('chat message', {
+        //     message: this.state.message
+        // })
+        this.setState({ messages: [...this.state.messages, {username: this.socket.id, message: msg}] },function(){console.log(this.state.messages);});
         
     };
 
@@ -63,7 +66,7 @@ class SocketPage extends React.Component {
                     )
                 })}
                 <input type="text" value={this.state.name} onChange={this.changeMessageInLocalState}/>
-                <button onClick={this.addMessage}>Send</button>
+                <button onClick={this.sendMessage}>Send</button>
             </div>
         );
     }
